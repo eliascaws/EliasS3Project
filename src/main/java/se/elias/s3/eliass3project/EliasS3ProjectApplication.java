5a23884090f811f8d3f7779d86633a5a7a77a2f1
eliascaws <eliasfagerstrom@gmail.com>
2025-09-26 00:27:25 +0200

    Initial commit

---
+++ src/main/java/se/elias/s3/eliass3project/EliasS3ProjectApplication.java
@@ -0,0 +1,13 @@
+package se.elias.s3.eliass3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliasS3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EliasS3ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/elias/s3/eliass3project/EliasS3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.elias.s3.eliass3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliasS3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
